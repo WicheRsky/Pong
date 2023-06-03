100a77abd50819f7198232b4d2721f666570ad6e
WicheRsky <ilt19991@wp.pl>
2023-06-03 12:37:38 +0200

    projectimport

---
+++ Assets/BallController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    public Rigidbody2D rb2D;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+        rb2D.velocity = Vector3.
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
