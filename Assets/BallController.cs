bf1c973921541930049ce2f1bbe2aeea4acb06d9
WicheRsky <ilt19991@wp.pl>
2023-06-29 13:45:13 +0200

    Finish

--- Assets/BallController.cs
+++ Assets/BallController.cs
@@ -4,18 +4,74 @@ using UnityEngine;
 
 public class BallController : MonoBehaviour
 {
-    public Rigidbody2D rb2D;
+    public Rigidbody2D rbody2D;
+    public float speed = 9;
+    public Vector3 vel;
+    public bool isPlaying;
 
-    // Start is called before the first frame update
     void Start()
     {
-        rb2D = GetComponent<Rigidbody2D>();
-        rb2D.velocity = Vector3.
+        rbody2D = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Input.GetKey(KeyCode.Space) && isPlaying == false)
+        {
+            ResetAndSendBallInRandomDirection();
+        }
+        if (rbody2D.velocity.magnitude < speed * 0.5f)
+        {
+            ResetBall();
+        }
+    }
+
+    private void ResetBall()
+    {
+        rbody2D.velocity = Vector3.zero;
+        transform.position = Vector3.zero;
+        isPlaying = false;
+    }
+
+    private void ResetAndSendBallInRandomDirection()
+    {
+        ResetBall();
+        rbody2D.velocity = GenerateRandomVelocity(true) * speed;
+        vel = rbody2D.velocity;
+        isPlaying = true;
+    }
+
+    private Vector3 GenerateRandomVelocity(bool shouldReturnNormalized)
+    {
+        Vector3 velocity = new Vector3();
+        bool shouldGoRight = Random.Range(1, 100) > 50;
+        bool shouldGoLeft = Random.Range(1, 100) < 50;
+        velocity.x = shouldGoRight ? Random.Range(-.3f, -.1f): Random.Range(.3f, .1f);
+        velocity.y = shouldGoLeft ? Random.Range(-.3f, -.1f): Random.Range(.3f, .1f);
+
+        if (shouldReturnNormalized)
+        {
+            return velocity.normalized;
+        }
+        return velocity;
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        rbody2D.velocity = Vector3.Reflect(vel, collision.contacts[0].normal);
+        Vector3 newVelocityWithOffset = rbody2D.velocity;
+        newVelocityWithOffset += new Vector3(Random.Range(-.5f, .5f), Random.Range(-.5f, .5f));
+        rbody2D.velocity = newVelocityWithOffset.normalized * speed;
+        vel = rbody2D.velocity;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (transform.position.x > 0)
+            print("Lewy +1");
+        if (transform.position.x < 0)
+            print("Prawy +1");
+        ResetBall();
     }
 }
--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -9,13 +9,11 @@ public class PlayerController : MonoBehaviour
     public KeyCode upKey = KeyCode.W;
     public KeyCode downKey = KeyCode.S;
 
-    // Start is called before the first frame update
     void Start()
     {
         Debug.Log("Start");
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetKey(upKey) && transform.position.y < 5)
